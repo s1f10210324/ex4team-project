344a40f8596509336163270fd0cd2deb3702c077
s1f10210324 <s1f102103246@iniad.org>
2022-12-15 15:17:37 +0900

    change

--- django1/config/urls.py
+++ django1/config/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('chat/', include("chat.urls")),
+    path('study/', include("study.urls")),
     path('admin/', admin.site.urls),
 ]
